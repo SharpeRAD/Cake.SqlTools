d482247919ace9c859fb6350d3f4a2ff6bfd7c8c
SharpeRAD <code@sharperad.com>
2017-10-01 13:02:13 +0100

    Remove old projects

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,9 +5,8 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyProduct("Cake.SqlTools")]
-[assembly: AssemblyVersion("0.0.7")]
-[assembly: AssemblyFileVersion("0.0.7")]
-[assembly: AssemblyInformationalVersion("0.0.7")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 - 2017 Phillip Sharpe")]
 
